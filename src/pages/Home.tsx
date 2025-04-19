661b322cf9d3cdc80630dc7b0a39e711948a3de8
Vincent <s232546@student.dhbw-mannheim.de>
2025-04-19 19:33:57 +0200

    implemented Header

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,35 @@
 import '../App.css'
+import React, { useEffect, useState} from 'react'
 
 function Home() {
+
+  const [zeit, setZeit] = useState(new Date())
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setZeit(new Date())
+    }, 1000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  const datumUhrzeit = zeit.toLocaleString('de-DE', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  })
+
   return (
     <>
-      <h1>Willkommen im Supermarkt von Gruppe 4!</h1>
+      <header className='header'>
+        <div className='logoContainer'> 
+        </div>
+        <p className='datumUhrzeit'>Supermarktkasse {datumUhrzeit}</p>
+      </header>
     </>
   )
 }
